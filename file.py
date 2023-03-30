4434772c25990d1e13eff9005b64cb9f88d5ee94
Dwisler <tymah4er@gmail.com>
2023-03-30 14:51:40 +0300

    update code to output all data from pdf in rows where each row begins with date

--- file.py
+++ file.py
@@ -12,21 +12,16 @@ for page in range(len(reader.pages)):
     page_obj = reader.pages[page]
     data += page_obj.extract_text()
 
-pattern = r'(\d{2}\.\d{2}\.\d{2})\s+([\+\-])\s+([\d\s]+,\d+)\s+₸\s+([^\n]+)\n(\([^\)]+\))?\s*([^\n]+)?'
+# Find the data in pdf after the line "Дата Сумма Операция Детали"
+match = re.search(r'Дата Сумма Операция\s+Детали\s+(.+)', data, re.DOTALL)
+data_after_header = match.group(1) if match else ''
 
-result = re.findall(pattern,data)
+# Divide text to rows by date
+lines = re.split(r'(?=\d{2}\.\d{2}\.\d{2})', data_after_header)
+
 
 with open(os.path.expanduser('~/Desktop/docs/kaspi_output.csv'), 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['Date', 'Amount', 'Operation', 'Description', 'Currency Amount 1'])
-    
-    for row in result:
-        date = row[0]
-        operand = row[1]
-        amount = row[2]
-        description_parts = re.split(r'\s{3,}', row[3])
-        operation = description_parts[0]
-        description = description_parts[1] if len(description_parts) > 1 else ''
-        currency_amount = row[4] if row[4] else ''
-        
-        writer.writerow([date, operand + amount, operation, description, currency_amount])
+
+    for line in lines:
+        writer.writerow([line])
