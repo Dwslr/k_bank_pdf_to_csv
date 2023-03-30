ad17ec8891c69204dabe688bb2cd17d41dd73622
Dwisler <tymah4er@gmail.com>
2023-03-30 15:21:27 +0300

    csv output contains common column and correct specify columns: date, amount(+/- tenge), operation, note, amount2, currency

--- file.py
+++ file.py
@@ -23,17 +23,40 @@ lines = re.split(r'(?=\d{2}\.\d{2}\.\d{2})', data_after_header)
 with open(os.path.expanduser('~/Desktop/docs/kaspi_output.csv'), 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
 
+    # Write the header row
+    writer.writerow(['line', 'date', 'amount', 'operation', 'note', 'amount2', 'currency'])
+
     for line in lines:
         if line.strip() and ' ' in line:
            # Extract the date using the split() method
-            date = line.split()[0] 
+            date = line.split()[0]
+
             # Use a regular expression to extract the amount
             match = re.search(r'([+-].+₸)', line)
             amount = match.group(1) if match else ''
+            # Delete spaces, change , sign to . sign, delete the tenge symbol, and delete the + operand
+            amount = amount.replace(' ', '').replace(',', '.').replace('₸', '').replace('+', '')
+            
             # Use a regular expression to extract the operation
             match = re.search(r'\S+\s+\S+\s{2,}(\S+)', line)
             operation = match.group(1) if match else ''
+            
+            # Use a regular expression to extract the note
+            match = re.search(r'\S+\s+\S+\s{2,}\S+\s{3,}(.+)', line)
+            note = match.group(1) if match else ''
+            
+            # Use a regular expression to extract the currency
+            match = re.search(r'\((.+?)\)', line)
+            currency = match.group(1) if match else ''
+            # Check if the currency value is empty
+            if currency:
+                # Split the currency value by space
+                amount2, currency = currency.split()
+            else:
+                amount2 = ''
+                currency = ''
+
             # Write the data to the CSV file
-            writer.writerow([line, date, amount, operation])
+            writer.writerow([line, date, amount, operation, note, amount2, currency])
         else:
             writer.writerow([line])
