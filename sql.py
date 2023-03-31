6f5aeb95da6d073607ac80f0d105efc08f4dd683
Dwisler <tymah4er@gmail.com>
2023-03-31 15:29:41 +0300

    remake the code for 2 dataframes (kaspi and currencies) and check their output in console

--- sql.py
+++ sql.py
@@ -9,59 +9,33 @@ import os
 
 # Create DataFrame (df) from csv file
 df_kaspi = pd.read_csv(os.path.expanduser('~/Desktop/docs/kaspi_output.csv'))
-
-# print(df.head())
+df_curr = pd.read_excel(os.path.expanduser('~/Desktop/docs/cur_rates_last_year.xlsx'))
 
 # Correct date format in df
 df_kaspi['date'] = pd.to_datetime(df_kaspi['date'],format='%d-%m-%Y')
-# print(df.dtypes)
+df_curr['day'] = pd.to_datetime(df_curr['day'],format='%d-%m-%Y')
 
 # Delete the first column from the DataFrame
 df_kaspi = df_kaspi.iloc[:, 1:]
-
 # Delete the empty row from the DataFrame
 df_kaspi = df_kaspi.drop(df_kaspi.index[0])
 
-# Connect data to the SQLite database
-con1 = sqlite3.connect(os.path.expanduser('~/Desktop/docs/kaspi_dm.db'))
-df_kaspi.to_sql('kaspi_dm',con1,index=False,if_exists='replace')
+# Check
+print(df_kaspi.head())
+print(df_curr.head())
+print(df_kaspi.dtypes)
+print(df_curr.dtypes)
 
-# Create function 'select' with parameter 'sql'
-def select(sql, con):
-  """print the query result"""
-  return print(pd.read_sql(sql, con))
 
-# Declaration parameter
-# sql = '''
-# SELECT STRFTIME("%Y-%m", date) AS year_month, SUM(amount)
-# FROM kaspi_dm AS t
-# WHERE amount > 0
-# GROUP BY 1
-# ORDER BY 1 DESC
-# '''
 
-#Function call
-# select(sql)
 
-# Create DataFrame (df) from excel file with currencies
-df_curr = pd.read_excel(os.path.expanduser('~/Desktop/docs/cur_rates_last_year.xlsx'))
 
-df_curr['day'] = pd.to_datetime(df_curr['day'],format='%d-%m-%Y')
-# print(df_curr.dtypes)
-# print(df_curr.head())
 
-# Connect data from currencies DataFrame to the SQLite database
-con2 = sqlite3.connect(os.path.expanduser('~/Desktop/docs/curr.db'))
-df_curr.to_sql('curr',con2,index=False,if_exists='replace')
+# # Connect data to the SQLite database
+# con = sqlite3.connect(os.path.expanduser('~/Desktop/docs/kaspi_dm.db'))
+# df_kaspi.to_sql('kaspi_dm',con1,index=False,if_exists='replace')
 
-sql = '''
-SELECT *
-FROM kaspi_dm'''
-
-select(sql, con1)
-
-sql = '''
-SELECT *
-FROM curr'''
-
-select(sql, con2)
+# # Create function 'select' with parameter 'sql'
+# def select(sql, con):
+#   """print the query result"""
+#   return print(pd.read_sql(sql, con))
