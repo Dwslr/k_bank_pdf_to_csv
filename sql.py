c274051e18faba2d637f8848b4effdfaa0a43679
Dwisler <tymah4er@gmail.com>
2023-03-31 11:47:22 +0300

    add some comments to the code. now its time to make queries to db

--- sql.py
+++ sql.py
@@ -2,14 +2,17 @@ import sqlite3
 import pandas as pd
 # I dont know yet why I import numpy
 import numpy as np
+# Import os to create paths like this '~/Desktop/...'
+# use code  os.path.expanduser('~/...')
 import os
 
 
-
+# Create DataFrame (df) from csv file
 df = pd.read_csv(os.path.expanduser('~/Desktop/docs/kaspi_output.csv'))
 
 # print(df.head())
 
+# Correct date format in df
 df['date'] = pd.to_datetime(df['date'],format='%d-%m-%Y')
 # print(df.dtypes)
 
@@ -23,11 +26,15 @@ df = df.drop(df.index[0])
 con = sqlite3.connect(os.path.expanduser('~/Desktop/docs/kaspi_dm.db'))
 df.to_sql('kaspi_dm',con,index=False,if_exists='replace')
 
+# Create function 'select' with parameter 'sql'
 def select(sql):
+  """print the query result"""
   return print(pd.read_sql(sql,con))
 
+# Declaration parameter
 sql = '''
 SELECT *
 FROM kaspi_dm
 '''
+#Function call
 select(sql)
