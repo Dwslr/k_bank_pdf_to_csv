63eb3bd1771945fc6dddb2521f855f7d1609729d
Dwisler <tymah4er@gmail.com>
2023-04-01 20:41:21 +0300

    change the source DF from FFin bank. cant understand how to delete canceled financial transactions like 38589.56 2023-02-23

--- sql.py
+++ sql.py
@@ -14,33 +14,31 @@ df_ffin = pd.read_excel(os.path.expanduser('~/Desktop/docs/ffinkz_statement.xlsx
 
 # print(df_ffin.columns)
 
-# Drop unnamed columns
-cols_to_drop = [col for col in df_ffin.columns if col.startswith('Unnamed:')]
+# Drop column
+cols_to_drop = [col for col in df_ffin.columns if col.startswith('amount_curr')]
 df_ffin = df_ffin.drop(cols_to_drop, axis=1)
 
-# Convert columns from text in float
-df_ffin[['income', 'outcome', 'amount_curr']] = df_ffin[['income', 'outcome', 'amount_curr']].apply(lambda x: x.str.replace(r'\s', '', regex=True).str.replace(',', '.', regex=False).astype(float))
 # Convert column in desired date format 
-df_ffin[['date_op', 'date_deb']] = df_ffin[['date_op', 'date_deb']].apply(lambda x: pd.to_datetime(x, format='%d.%m.%Y %H:%M:%S').dt.strftime('%Y-%m-%d'))
+df_ffin[['date']] = df_ffin[['date']].apply(lambda x: pd.to_datetime(x, format='%Y.%m.%d %H:%M:%S').dt.strftime('%Y-%m-%d'))
 
 # Correct date format in df
 df_kaspi['date'] = pd.to_datetime(df_kaspi['date'],format='%d-%m-%Y')
 df_curr['day'] = pd.to_datetime(df_curr['day'],format='%d-%m-%Y')
-df_ffin['date_op'] = pd.to_datetime(df_ffin['date_op'],format='%Y-%m-%d')
-df_ffin['date_deb'] = pd.to_datetime(df_ffin['date_deb'],format='%Y-%m-%d')
+# df_ffin['date_op'] = pd.to_datetime(df_ffin['date_op'],format='%Y-%m-%d')
+df_ffin['date'] = pd.to_datetime(df_ffin['date'],format='%Y-%m-%d')
 
 # Delete the first column from the DataFrame
 df_kaspi = df_kaspi.iloc[:, 1:]
 # Delete the empty row from the DataFrame
 df_kaspi = df_kaspi.drop(df_kaspi.index[0])
-df_ffin = df_ffin.drop(df_ffin.index[0])
+# df_ffin = df_ffin.drop(df_ffin.index[0])
 
 # Check
-# print(df_ffin.head())
 # print(df_kaspi.head())
 # print(df_curr.head())
 # print(df_kaspi.dtypes)
 # print(df_curr.dtypes)
+# print(df_ffin.head())
 # print(df_ffin .dtypes)
 
 # Create a connection to the SQLite database
@@ -76,7 +74,6 @@ def select(sql):
 
 sql = '''
 SELECT *
-FROM ffin
-'''
+FROM ffin AS f'''
 
 select(sql)
