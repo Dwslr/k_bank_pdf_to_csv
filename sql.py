22dad330707b30a1e155bc25371a61bcbf99e1e3
Dwisler <tymah4er@gmail.com>
2023-04-02 11:43:00 +0300

    define rejected transactions in ffin

--- sql.py
+++ sql.py
@@ -19,7 +19,7 @@ cols_to_drop = [col for col in df_ffin.columns if col.startswith('amount_curr')]
 df_ffin = df_ffin.drop(cols_to_drop, axis=1)
 
 # Convert column in desired date format 
-df_ffin[['date']] = df_ffin[['date']].apply(lambda x: pd.to_datetime(x, format='%Y.%m.%d %H:%M:%S').dt.strftime('%Y-%m-%d'))
+#df_ffin[['date']] = df_ffin[['date']].apply(lambda x: pd.to_datetime(x, format='%Y.%m.%d %H:%M:%S').dt.strftime('%Y-%m-%d'))
 
 # Correct date format in df
 df_kaspi['date'] = pd.to_datetime(df_kaspi['date'],format='%d-%m-%Y')
@@ -33,6 +33,8 @@ df_kaspi = df_kaspi.iloc[:, 1:]
 df_kaspi = df_kaspi.drop(df_kaspi.index[0])
 # df_ffin = df_ffin.drop(df_ffin.index[0])
 
+
+
 # Check
 # print(df_kaspi.head())
 # print(df_curr.head())
@@ -45,7 +47,7 @@ df_kaspi = df_kaspi.drop(df_kaspi.index[0])
 con = sqlite3.connect(os.path.expanduser('~/Desktop/docs/database_kc.db'))
 
 # Create cursor object to execute SQL commands ???
-# cursor = con.cursor()
+cursor = con.cursor()
 
 # Convert the dataframes into SQLite tables using the to_sql method
 df_kaspi.to_sql('kaspi',con,index=False,if_exists='replace')
@@ -57,23 +59,15 @@ def select(sql):
   """print the query result"""
   return print(pd.read_sql(sql, con))
 
-# sql = '''
-# SELECT k.date, IIF(amount < 0, ROUND(amount / rate_rub_kzt, 0), NULL) AS outcome_rub, IIF(amount > 0, ROUND(amount / rate_rub_kzt, 0), NULL) AS income_rub
-# FROM kaspi AS k
-# JOIN curr AS c ON k.date = c.day
-# '''
-
-# sql ='''
-# SELECT strftime('%Y-%m', date), SUM(outcome_rub), SUM(income_rub)
-# FROM (SELECT k.date, IIF(amount < 0, ROUND(amount / rate_rub_kzt, 0), NULL) AS outcome_rub, IIF(amount > 0, ROUND(amount / rate_rub_kzt, 0), NULL) AS income_rub
-#     FROM kaspi AS k
-#     JOIN curr AS c ON k.date = c.day)
-# GROUP BY 1
-# ORDER BY 1 DESC
-# '''
-
+# Select the operations that were declined. The number of these operations is even
 sql = '''
-SELECT *
-FROM ffin AS f'''
+SELECT COUNT(date), ABS(amount), date
+FROM ffin
+GROUP BY 2
+HAVING COUNT(date) > 1 AND COUNT(date) % 2 = 0
+ORDER BY 3 DESC
+'''
+
+
 
 select(sql)
